7e870791d1c9507574fcbf9a08cd33b3d8c540e3
zouharidyaeerrahmane <dyae.zouhari-etu@etu.univh2c.ma>
2026-05-07 16:41:28 +0100

    Feat : Add Swagger test via swagger et postman

---
+++ src/main/java/org/java/enset/dezy/ebanking_backend/web/CustomerRestController.java
@@ -0,0 +1,46 @@
+package org.java.enset.dezy.ebanking_backend.web;
+
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.java.enset.dezy.ebanking_backend.dtos.CustomerDTO;
+import org.java.enset.dezy.ebanking_backend.entities.Customer;
+import org.java.enset.dezy.ebanking_backend.exceptions.CustomerNotFoundException;
+import org.java.enset.dezy.ebanking_backend.services.BankAccountService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@Slf4j
+@CrossOrigin("*")
+public class CustomerRestController {
+    private BankAccountService bankAccountService;
+
+    @GetMapping("/customers")
+    public List<CustomerDTO> customers(){
+        return bankAccountService.listCustomers();
+    }
+    @GetMapping("/customers/search")
+    public List<CustomerDTO> searchCustomers(@RequestParam(name = "keyword",defaultValue = "") String keyword){
+        return bankAccountService.searchCustomers("%"+keyword+"%");
+    }
+    @GetMapping("/customers/{id}")
+    public CustomerDTO getCustomer(@PathVariable(name = "id") Long customerId) throws CustomerNotFoundException {
+        return bankAccountService.getCustomer(customerId);
+    }
+    @PostMapping("/customers")
+    public CustomerDTO saveCustomer(@RequestBody CustomerDTO customerDTO){
+        return bankAccountService.saveCustomer(customerDTO);
+    }
+    @PutMapping("/customers/{customerId}")
+    public CustomerDTO updateCustomer(@PathVariable Long customerId, @RequestBody CustomerDTO customerDTO){
+        customerDTO.setId(customerId);
+        return bankAccountService.updateCustomer(customerDTO);
+    }
+    @DeleteMapping("/customers/{id}")
+    public void deleteCustomer(@PathVariable Long id){
+        bankAccountService.deleteCustomer(id);
+    }
+}
